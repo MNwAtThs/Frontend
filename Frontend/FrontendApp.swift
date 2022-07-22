afbe42838bc786362e2426ec02161acf750ee76b
Angelo <angelo@getohsome.com>
2022-07-22 03:42:08 +0200

    Client changes

--- Frontend/FrontendApp.swift
+++ Frontend/FrontendApp.swift
@@ -11,19 +11,21 @@ import Client
 @main
 struct FrontendApp: App {
     let client = Client(baseURL: URL(string: "http://localhost:8080")!)
+        .withAuthProvider(BearerAuthProvider(), forAuthType: .bearer)
     
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .task {
-                    do {
-                        let response = try await client.createPost()
-                        print(response)
-                    }
-                    catch {
-                        print(error)
-                    }
-                }
         }
     }
 }
+
+
+public class BearerAuthProvider: AuthProvider {
+    public func handleAuth(forRequest urlRequest: inout URLRequest) -> AuthProviderResult {
+        let value = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyMjhEOEY5Qy1BODRDLTQwRjMtODk1OC03OUNGNEI1RDVCMDciLCJqdGkiOiI3MzYxQUEzNy1COENELTQxQkQtOUM1MC1FQjkyN0IxN0MwMzkifQ.WxcMMloVB1Q9GmBL4IKsNVerOjoArwSuKrPlVzqDpJg"
+        
+        return .success("Bearer \(value)")
+    }
+}
+
